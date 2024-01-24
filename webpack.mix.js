4ac090c1a09fbb2157e2a02f5d95f8f53938cf0d
angy <angeyimportante780@gmail.com>
2024-01-24 08:48:32 +0100

    Iniziato ad aggiungere la grafica di AdminLTE. Questo commit viene fatto per poter migrare sul webserver

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,2 +1,5 @@
 mix.js('node_modules/bootstrap/dist/js/bootstrap.bundle.min.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css');
+mix.copy('node_modules/admin-lte/dist/css/adminlte.min.css', 'public/css/adminlte.min.css');
+mix.copy('node_modules/admin-lte/dist/js/adminlte.min.js', 'public/js/adminlte.min.js');
+
